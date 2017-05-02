dd7c41c08e30718441ea1a459024601dabab6982
Christian Bryn <chr.bryn@gmail.com>
2017-05-03 01:12:28 +0200

    add exception handling for socket connect
    ...for when connecting to carbon server.
    Also: +++

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# MQTT Subscriber for posting JSON metric data to a graphite (carbon) server
+# Christian Bryn <chr.bryn@gmail.com> 20117
 # -*- coding: utf-8 -*-
 
 #import paho.mqtt.client as mqtt
@@ -10,14 +12,15 @@ import sys, os, socket, json, time
 DEBUG = True
 
 config = { 'mqtt_server': 'sasha', 'mqtt_topics': { 'arduino/bedroom/sensor2': ['moisture'] }, 'carbon_server': 'brynaws', 'carbon_port': 2003 }
-#config = { 'mqtt_server': 'sasha', 'topic': 'arduino/bedroom/sensor2', 'carbon_server': 'brynaws', 'carbon_port': 2003 }
 
 def on_message_print(client, userdata, message):
+    """ Print received MQTT message to stdout """
     print("%s %s" % (message.topic, message.payload))
 
 def on_message_carbon(client, userdata, message):
     """ Send MQTT JSON metric objects to carbon server """
-    print(message.payload)
+    if DEBUG:
+        print(message.payload)
     data = json.loads(message.payload.decode("utf-8"))
     for metric in config['mqtt_topics'][message.topic]:
         value = data['moisture']
@@ -30,26 +33,22 @@ def on_message_carbon(client, userdata, message):
             print(message.topic)
             print(payload)
             return
-        sock = socket.socket()
-        sock.connect((config['carbon_server'], config['carbon_port']))
-        sock.sendall(payload.encode())
-        sock.close()
+        try:
+            sock = socket.socket()
+            sock.connect((config['carbon_server'], config['carbon_port']))
+            sock.sendall(payload.encode())
+            sock.close()
+        except OSError as e:
+            print("Could not connect to host %s:%s: %s" % (config['carbon_server'], config['carbon_port'], e))
 
+def main():
+    if DEBUG:
+        print(config['mqtt_server'])
+        print(config['mqtt_topics'])
 
+    subscribe.callback(on_message_carbon, [x for x in config['mqtt_topics']], hostname=config['mqtt_server'])
 
-
-print(config['mqtt_server'])
-print(config['mqtt_topics'])
-
-#if DEBUG:
-#    subscribe.callback(on_message_print, config['topic'], hostname=config['server'])
-#else:
-#    subscribe.callback(on_message_carbon, config['topic'], hostname=config['server'])
-#
-#for topic in 
-#subscribe.callback(on_message_carbon, config['topic'], hostname=config['mqtt_server'])
-
-for topic in config['mqtt_topics']:
-    subscribe.callback(on_message_carbon, topic, hostname=config['mqtt_server'])
+if __name__ == "__main__":
+    main()
 
 
